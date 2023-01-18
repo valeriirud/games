ea298d54508a45a7ca6e8a431764b0c0f65a222d
valeriirud <valerii.rud.personal@gmail.com>
2023-01-17 23:23:26 -0500

    dialer completed

--- Games/Pages/PokerBase.cs
+++ Games/Pages/PokerBase.cs
@@ -6,75 +6,96 @@ public class PokerBase : ComponentBase
 {
     public string[,] Images { get; }  = new string[9, 5];
     public string CroupierImage { get; set; } = string.Empty;
-
-    readonly List<int> _indexes = new();
-    readonly string[] _images = 
+    public string ImageDialer { get; } = @"images\dialer.svg";
+    public int DialerId { get; set; } = -1;
+    readonly List<Tuple<int,int>> _indexes = new();
+    readonly List<Tuple<int, int>>[] _hands = new List<Tuple<int, int>>[9];
+    readonly string[,] _images = 
         {
-            @"images\clubs_2.svg",
-            @"images\clubs_3.svg",
-            @"images\clubs_4.svg",
-            @"images\clubs_5.svg",
-            @"images\clubs_6.svg",
-            @"images\clubs_7.svg",
-            @"images\clubs_8.svg",
-            @"images\clubs_9.svg",
-            @"images\clubs_10.svg",
-            @"images\clubs_jack.svg",
-            @"images\clubs_queen.svg",
-            @"images\clubs_king.svg",
-            @"images\clubs_ace.svg",
-            @"images\diamonds_2.svg",
-            @"images\diamonds_3.svg",
-            @"images\diamonds_4.svg",
-            @"images\diamonds_5.svg",
-            @"images\diamonds_6.svg",
-            @"images\diamonds_7.svg",
-            @"images\diamonds_8.svg",
-            @"images\diamonds_9.svg",
-            @"images\diamonds_10.svg",
-            @"images\diamonds_jack.svg",
-            @"images\diamonds_queen.svg",
-            @"images\diamonds_king.svg",
-            @"images\diamonds_ace.svg",
-            @"images\hearts_2.svg",
-            @"images\hearts_3.svg",
-            @"images\hearts_4.svg",
-            @"images\hearts_5.svg",
-            @"images\hearts_6.svg",
-            @"images\hearts_7.svg",
-            @"images\hearts_8.svg",
-            @"images\hearts_9.svg",
-            @"images\hearts_10.svg",
-            @"images\hearts_jack.svg",
-            @"images\hearts_queen.svg",
-            @"images\hearts_king.svg",
-            @"images\hearts_ace.svg",
-            @"images\spades_2.svg",
-            @"images\spades_3.svg",
-            @"images\spades_4.svg",
-            @"images\spades_5.svg",
-            @"images\spades_6.svg",
-            @"images\spades_7.svg",
-            @"images\spades_8.svg",
-            @"images\spades_9.svg",
-            @"images\spades_10.svg",
-            @"images\spades_jack.svg",
-            @"images\spades_queen.svg",
-            @"images\spades_king.svg",
-            @"images\spades_ace.svg"
+            {
+                @"images\clubs_2.svg",
+                @"images\clubs_3.svg",
+                @"images\clubs_4.svg",
+                @"images\clubs_5.svg",
+                @"images\clubs_6.svg",
+                @"images\clubs_7.svg",
+                @"images\clubs_8.svg",
+                @"images\clubs_9.svg",
+                @"images\clubs_10.svg",
+                @"images\clubs_jack.svg",
+                @"images\clubs_queen.svg",
+                @"images\clubs_king.svg",
+                @"images\clubs_ace.svg" 
+            },
+            {
+                @"images\diamonds_2.svg",
+                @"images\diamonds_3.svg",
+                @"images\diamonds_4.svg",
+                @"images\diamonds_5.svg",
+                @"images\diamonds_6.svg",
+                @"images\diamonds_7.svg",
+                @"images\diamonds_8.svg",
+                @"images\diamonds_9.svg",
+                @"images\diamonds_10.svg",
+                @"images\diamonds_jack.svg",
+                @"images\diamonds_queen.svg",
+                @"images\diamonds_king.svg",
+                @"images\diamonds_ace.svg" 
+            },
+            {
+                @"images\hearts_2.svg",
+                @"images\hearts_3.svg",
+                @"images\hearts_4.svg",
+                @"images\hearts_5.svg",
+                @"images\hearts_6.svg",
+                @"images\hearts_7.svg",
+                @"images\hearts_8.svg",
+                @"images\hearts_9.svg",
+                @"images\hearts_10.svg",
+                @"images\hearts_jack.svg",
+                @"images\hearts_queen.svg",
+                @"images\hearts_king.svg",
+                @"images\hearts_ace.svg"
+            },
+            {
+                @"images\spades_2.svg",
+                @"images\spades_3.svg",
+                @"images\spades_4.svg",
+                @"images\spades_5.svg",
+                @"images\spades_6.svg",
+                @"images\spades_7.svg",
+                @"images\spades_8.svg",
+                @"images\spades_9.svg",
+                @"images\spades_10.svg",
+                @"images\spades_jack.svg",
+                @"images\spades_queen.svg",
+                @"images\spades_king.svg",
+                @"images\spades_ace.svg"
+            }
         };
+
+    void Inint()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            _hands[i] = new ();
+        }
+    }
     public async Task NewGame()
     {
+        Inint();    
         Clear();
         await DealerSelection();
-        //int index = GetIndex();
-        //CroupierImage = _images[index];
+        //Tuple<int,int> index = GetIndex();
+        //CroupierImage = _images[index.Item1, index.Item2];
         //for (int i = 0; i < 9; i++)
         //{
         //    for (int j = 0; j < 5; j++)
         //    {
         //        index = GetIndex();
-        //        Images[i, j] = _images[index];
+        //        Images[i, j] = _images[index.Item1, index.Item2];
+        //        await Task.Delay(100);
+        //        StateHasChanged();
         //    }
         //}
     }
@@ -85,44 +106,112 @@ public class PokerBase : ComponentBase
         CroupierImage = string.Empty;
         for(int i = 0; i < 9; i ++)
         {
+            _hands[i].Clear();
             for (int j = 0; j < 5; j++)
             {
                 Images[i,j] = string.Empty;
             }
         }
+        DialerId = -1;
     }
 
-    int GetIndex()
+    Tuple<int, int> GetIndex()
     {
-        int index = -1;
-        while (index < 0)
+        int index1 = -1;
+        int index2 = -1;
+        while (index1 < 0 || index2 < 0)
         {
             Random r = new Random();
-            index = r.Next(0, _images.Length - 1);
-            if(_indexes.Contains(index))
+            if (index1 < 0)
             {
-                index = -1;
+                index1 = r.Next(0, _images.GetUpperBound(0) + 1);
+            }
+            if (index2 < 0)
+            {
+                index2 = r.Next(0, _images.GetUpperBound(1) + 1);
+            }
+            if (_indexes.Any(i => i.Item1 == index1 && i.Item2 == index2))
+            {
+                index1 = -1;
+                index2 = -1;
             }
             else
             {
-                _indexes.Add(index);
+                _indexes.Add(new(index1, index2));
             }
         }
-        return index;
+        return new (index1, index2);
     }
 
     async Task DealerSelection()
     {
-        for (int i = 0; i < 9; i++)
+        int id = 0;
+        while (true)
         {
-            int index = GetIndex();
-            CroupierImage = _images[index];
-            StateHasChanged();
-            await Task.Delay(200);
-            CroupierImage = string.Empty;
-            StateHasChanged();
-            Images[i, 0] = _images[index];
-            StateHasChanged();
+            for (int i = 0; i < 9; i++)
+            {
+                Tuple<int, int> index = GetIndex();
+                CroupierImage = _images[index.Item1, index.Item2];
+                StateHasChanged();
+                await Task.Delay(200);
+                CroupierImage = string.Empty;
+                StateHasChanged();
+                Images[i, 0] = _images[index.Item1, index.Item2];
+                _hands[i].Add(index);
+                StateHasChanged();
+            }
+            List<int> maxHands = GetMaxHands();
+            await Task.Delay(2000);
+            if (maxHands.Count > 1)
+            {
+                Clear();
+                continue;
+            }
+            id = maxHands[0];
+            break;
         }
+        await Task.Delay(5000);
+        StateHasChanged();
+        Clear();
+        DialerId = id;        
+    }
+
+    List<int> GetMaxHands()
+    {
+        int maxHand = -1;
+        List<int> maxHands = new() { 0 };
+        for(int i = 1; i < 9; i ++)
+        {
+            maxHand = CompareHands(_hands[maxHands[0]], _hands[i]);
+            if(maxHand == 0)
+            {
+                maxHands.Add(i);
+            }
+            else if(maxHand == 2)
+            {
+                maxHands.Clear();
+                maxHands.Add(i);
+            }
+        }
+
+        return maxHands;
+    }
+
+    int CompareHands(List<Tuple<int, int>> hand1, List<Tuple<int, int>> hand2)
+    {
+        if (hand1.Count == 1) return CompareOne(hand1, hand2);
+        return CompareMany(hand1, hand2);
+    }
+
+    int CompareOne(List<Tuple<int, int>> hand1, List<Tuple<int, int>> hand2)
+    {
+        if (hand1[0].Item2 == hand2[0].Item2) return 0;
+        if (hand1[0].Item2 > hand2[0].Item2) return 1;
+        return 2;
+    }
+
+    int CompareMany(List<Tuple<int, int>> hand1, List<Tuple<int, int>> hand2)
+    {
+        return 0;
     }
 }
